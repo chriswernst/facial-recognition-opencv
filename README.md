b96a21bb366f238e03d702cc209392a8a340c054
Chris Ernst <chriswernst@gmail.com>
2017-09-26 16:02:48 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Facial Recognition
+
+#### Overview
+
+[//]: # (Image References)
+
+[image1]: ./images/apple.jpg
+[image2]: ./images/applefaceDetect.jpg
+[image3]: ./images/chrysler.jpg
+[image4]: ./images/chryslerfaceDetect.jpg
+[image5]: ./images/edison.jpg
+[image6]: ./images/edisonfaceDetect.jpg
+[image7]: ./images/elon.jpg
+[image8]: ./images/elonfaceDetect.jpg
+[image9]: ./images/guido.jpg
+[image10]: ./images/guidofaceDetect.jpg
+[image11]: ./images/sandberg.jpg
+[image12]: ./images/sandbergfaceDetect.jpg
+[image13]: ./images/willow.jpg
+[image14]: ./images/willowfaceDetect.jpg
+
+We will step through a brief overview of how to find faces in images using **Python 3.5**
+###
+![][image10]
+*(A tribute to the father of Python)*
+###
+We will leverage the following Python Modules:
+- OpenCV (cv2)
+- PIL
+- pylab
+- matplotlib
+- os
+
+OpenCV will do the majority of the heavy lifting. I've adopted [an example from the documentation, here.](http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_objdetect/py_face_detection/py_face_detection.html#face-detection)
+
+I would suggest you check out the documentation for implementation: http://docs.opencv.org
+###
+
---
+++ facialRecognition.py
@@ -0,0 +1,81 @@
+import cv2
+from PIL import Image
+from pylab import *
+import matplotlib.pyplot as plt
+import os
+
+
+def main():
+
+    try:
+        face_cascade = cv2.CascadeClassifier('/Users/ChrisErnst/anaconda/envs/py35/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+        eye_cascade = cv2.CascadeClassifier('/Users/ChrisErnst/anaconda/envs/py35/share/OpenCV/haarcascades/haarcascade_eye.xml')
+        # If you have trouble finding the xml files, search your computer and alter the path
+        
+        imageDirectory = '/Users/ChrisErnst/Development/Python/computerVision/facialRecognition/images/'
+        os.chdir(imageDirectory)
+        # Set the directory
+        
+        plt.figure('Facial Recognition')
+        plt.suptitle('Facial Recognition', size=20)
+        # Generate a figure named 'Face Recognition'
+        
+        imageName = 'apple.jpg'
+        # Set the image we want to analyze
+        
+        img = array(Image.open(imageName))
+        upperLeft = plt.subplot(221)
+        # The convention of 221 is the 1st image of a 2x2 figure
+        plt.axis('off')
+        plt.imshow(img)
+        upperLeft.set_title('Original Image')
+        # Show the original image using PIL and plot using pyplot in the upper left
+        
+        
+        gray = array(Image.open(imageName).convert('L'))
+        upperRight = plt.subplot(222)
+        plt.axis('off')
+        plt.imshow(gray, cmap='gray')
+        upperRight.set_title('Grayscale to Analyze')
+        # Convert to gray with PIL and plot using pyplot in the upper right
+        
+        
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        
+        for (x,y,w,h) in faces:
+            face = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+        
+        lowerLeft = plt.subplot(223)
+        plt.axis('off')
+        plt.imshow(face)
+        lowerLeft.set_title('Face Detection')
+        # Find the face in the photo, draw a red rectangle around it, and plot in the lower left
+        
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        
+        for (ex,ey,ew,eh) in eyes:
+            eye = cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            
+        lowerRight = plt.subplot(224)
+        plt.axis('off')
+        plt.imshow(eye)
+        lowerRight.set_title('Eye Detection')    
+        # Find the eyes in the photo, draw green rectangles around them, and plot in the lower right
+        
+        plt.savefig(imageDirectory + imageName[0:-4] +'faceDetect.jpg')
+        # Save the file    
+        
+    except UnboundLocalError:
+        plt.savefig(imageDirectory + imageName[0:-4] +'faceDetect.jpg')
+        # Save the file
+        print("Make sure there are faces and eyes in your image!")
+        # Exception for an image without faces and/or eyes
+        
+    except FileNotFoundError:
+        print("Image not found. Verify the file name and path!")
+        # Exception for bad image path
+            
+main()
+    
